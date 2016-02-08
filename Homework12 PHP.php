71d3b8110a898995fa12f45cb57f31e5a0b56b29
Vuyani <vuyani@Vuyanis-MacBook-Pro.local>
2016-02-08 10:12:46 +0200

    Added a title to the page

--- Homework12 PHP.php
+++ Homework12 PHP.php
@@ -3,6 +3,7 @@
   <title>PHP User</title>
  </head>
  <body>
+ <h1>INF 354 HW 12</h1>
  <?php echo '<p>u10230107 RH Matthews</p>'; ?> 
  <?php echo '<p>u11118271 AA Nyamela</p>'; ?> 
   <?php echo '<p>u10050061 DK Lehari</p>'; ?> 
